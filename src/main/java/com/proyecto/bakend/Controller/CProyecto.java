08a9eda3ce6a3becea240e6ec6227983c78277b9
Cecilia Miras <mirascecilia08@gmail.com>
2022-10-21 09:01:31 -0300

    make it better

--- src/main/java/com/proyecto/bakend/Controller/CProyecto.java
+++ src/main/java/com/proyecto/bakend/Controller/CProyecto.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/proyecto")
 @CrossOrigin(origins="http://localhost:4200")
-//@CrossOrigin(origins="https://frontend556-578de.web.app")
+
 public class CProyecto {
     @Autowired
     Sproyecto sProyecto;
--- src/main/java/com/proyecto/bakend/Security/Controller/AuthController.java
+++ src/main/java/com/proyecto/bakend/Security/Controller/AuthController.java
@@ -31,7 +31,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin(origins = "http://localhost:4200")
-//@CrossOrigin(origins = "https://frontend556-578de.web.app")
 
 public class AuthController {
 
